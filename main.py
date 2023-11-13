0fb01ba6c8fd88174acc7b6f21e332339ea53fa3
Dibyan Samantray <dibyansamantray@gmail.com>
2023-11-13 23:17:43 +0530

    Added respective function calls in main file to execute the animations.

--- main.py
+++ main.py
@@ -19,8 +19,26 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 
+#define fighter vairables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
 #load background image
 bg_image = pygame.image.load("2Dfighter_game/assets/images/background/background.jpg").convert_alpha()
+
+#load spritesheets
+warrior_sheet = pygame.image.load("2Dfighter_game/assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("2Dfighter_game/assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+#define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
 
 #function for drawing background
 def draw_bg():
@@ -35,8 +53,8 @@ def draw_health_bar(health, x, y):
     pygame.draw.rect(screen, YELLOW, (x, y, 400*ratio, 30))
 
 #create two instances of fighters
-fighter_1 = Fighter(200,310)
-fighter_2 = Fighter(700,310)
+fighter_1 = Fighter(1, 200, 310, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
+fighter_2 = Fighter(2, 700, 310, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
 
 #game loop
 run = True
@@ -54,6 +72,10 @@ while run:
     #move fighters
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
     #fighter_2.move()
+    
+    #update fighters
+    fighter_1.update()
+    fighter_2.update()
     
     #draw fighters
     fighter_1.draw(screen)
